89aeac13dada472cd8f2c2fa2dd6d44c96c6d274
Andrej03 <96391874+Andrej03@users.noreply.github.com>
2025-02-16 16:30:45 -0500

    error fixed,  db connection fixes

--- server.js
+++ server.js
@@ -6,40 +6,42 @@ const gqlSchema = require('./gqlSchema');
 const resolver = require('./resolver');
 
 // storing sensitive information
-const doenv = require('dotenv');
-doenv.config();
+const dotenv = require('dotenv');
+dotenv.config();
 
 const app = express();
 const SERVER_PORT = 3000;
 
-//MongoDB connection
+// MongoDB connection
 const mongoURL = process.env.MONGODB_URL;
 
-const connectDB = async() => {
-    try{
-      mongoose.connect(mongoURL, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-      }).then(_success => {
-        console.log('Success Mongodb connection')
-      }).catch(_err => {
-        console.log('Error Mongodb connection')
-      });
-    } catch(error) {
-        console.log(`Unable to connect to DB : ${error.message}`);
-      }
-}
+const connectDB = async () => {
+  try {
+    await mongoose.connect(mongoURL);
+    console.log('Success MongoDB connection');
+  } catch (error) {
+    console.log(`Unable to connect to DB: ${error.message}`);
+  }
+};
 
 // Apollo Server definition
 const apolloServer = new ApolloServer({
-    typeDefs: gqlSchema,
-    resolvers: resolver
-})
+  typeDefs: gqlSchema,
+  resolvers: resolver
+});
 
-apolloServer.applyMiddleware({ app });
+// Wrap startup code in an async function
+async function startServer() {
+  await apolloServer.start();
+  apolloServer.applyMiddleware({ app });
 
-// APP LISTENER
-app.listen(SERVER_PORT, () => {
-    console.log(`Server running on port http://localhost:${SERVER_PORT}/graphql`);
-    connectDB();
-})
+  // Connect to MongoDB
+  await connectDB();
+
+  // Start Express server
+  app.listen(SERVER_PORT, () => {
+    console.log(`Server running on port http://localhost:${SERVER_PORT}${apolloServer.graphqlPath}`);
+  });
+}
+
+startServer();
